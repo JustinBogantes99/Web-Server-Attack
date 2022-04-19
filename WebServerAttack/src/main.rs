00d918dde033bb24e5bc4602862f8d79351dbd5b
Justin Bogantes <justin.bogantes99@gmail.com>
2022-04-19 09:06:38 -0600

    .

--- WebServerAttack/src/main.rs
+++ WebServerAttack/src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io;//biblioteca para la salida estandar
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,7 +19,24 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line,filename)= 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK","index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND","404.html")
+        };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
